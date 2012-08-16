4f285e246514d961ae9973ee6612edab6fd1f612
Ali <alistair.edmunds@gmail.com>
2012-08-16 10:57:05 +0100

    Created application using Refinery CMS 2.0.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MelissaCms::Application.config.secret_token = '22296350d2cfbf9f82e08fbc7048a36bfd6988aa1de44b357de79cf465c25ea8048d24b90646b965582927b553fea6bb5f86c936132f54d25776bba3b37b2ea8'
